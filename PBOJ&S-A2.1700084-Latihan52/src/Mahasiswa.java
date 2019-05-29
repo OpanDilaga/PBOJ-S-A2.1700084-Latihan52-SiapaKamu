4b4c00aa276fba1ad9342c233bac27eff9bf7da5
OpanDilaga <51019063+OpanDilaga@users.noreply.github.com>
2019-05-29 13:22:04 +0700

    Add files via upload

---
+++ PBOJ&S-A2.1700084-Latihan52/src/Mahasiswa.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author acer
+ * Nama     : Opan Dilaga
+ * NIM      : A2.1700084
+ * Kelas    : TI IV E J&S
+ */
+public class Mahasiswa extends Manusia {
+    
+    private String nim;
+    private String kelas;
+    
+    public String getNim(){
+        return nim;
+    }
+    
+    public void setNim (String nim){
+        this.nim = nim;
+    }
+    
+    public String getKelas(){
+        return kelas;
+    }
+    
+    public void setKelas (String kelas){
+        this.kelas = kelas;
+        
+    }
+    
+    public void kelasApa(){
+        System.out.println("NIM  : "+nim);
+        System.out.println("saya mahasiswa");
+        System.out.println("saya  "+nama+" umur "+umur+"tahun "+"sedang belajar di kelas "+kelas);
+        
+    }
+}
